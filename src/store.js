39059a26a21e2fcda068122d76a14f3ddbaefbe5
shahbaz <tauheeds7888@gmail.com>
2023-03-28 19:51:40 +0530

    store reducer in store

--- src/store.js
+++ src/store.js
@@ -3,40 +3,30 @@
 import { combineReducers, applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import {
-	productListRedusers,
-	productDetailsRedusers,
-} from "./reducers/productReducers";
+import { productListRedusers, productDetailsRedusers } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
-import {
-	userLoginRedusers,
-	userRegisterRedusers,
-} from "./reducers/userReducers";
+import { userLoginRedusers, userRegisterRedusers, userDetailsRedusers, userUpdateProfileRedusers } from "./reducers/userReducers";
 
 const reducers = combineReducers({
-	productList: productListRedusers,
-	productDetails: productDetailsRedusers,
-	cart: cartReducer,
-	userLogin: userLoginRedusers,
-	userRegister: userRegisterRedusers,
+  productList: productListRedusers,
+  productDetails: productDetailsRedusers,
+  cart: cartReducer,
+  userLogin: userLoginRedusers,
+  userRegister: userRegisterRedusers,
+  userDetails: userDetailsRedusers,
+  userUpdateProfile: userUpdateProfileRedusers,
 });
-const cartItemsFromStorage = localStorage.getItem("cartItems")
-	? JSON.parse(localStorage.getItem("cartItems"))
-	: [];
+const cartItemsFromStorage = localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [];
 
-const userInfoFromStorage = localStorage.getItem("userInfo")
-	? JSON.parse(localStorage.getItem("userInfo"))
-	: null;
+const userInfoFromStorage = localStorage.getItem("userInfo") ? JSON.parse(localStorage.getItem("userInfo")) : null;
+
+const shippingAddressFromStorage = localStorage.getItem("shippingAddress") ? JSON.parse(localStorage.getItem("shippingAddress")) : {};
 
 const initialState = {
-	cart: { cartItems: cartItemsFromStorage },
-	userLogin: { userInfo: userInfoFromStorage },
+  cart: { cartItems: cartItemsFromStorage, shippingAddressFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 const middleWare = [thunk];
-const store = createStore(
-	reducers,
-	initialState,
-	composeWithDevTools(applyMiddleware(...middleWare))
-);
+const store = createStore(reducers, initialState, composeWithDevTools(applyMiddleware(...middleWare)));
 
 export default store;

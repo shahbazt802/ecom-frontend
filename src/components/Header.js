4e3277827fb244474a330b04833dcdc86f2e1ddc
shahbaz <tauheeds7888@gmail.com>
2023-03-28 19:52:25 +0530

    some changes in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,54 +9,52 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 function Header() {
-	const history = useNavigate();
-	const userLogin = useSelector((state) => state.userLogin);
-	const { userInfo } = userLogin;
-	const dispatch = useDispatch();
-	const logOutHandler = () => {
-		dispatch(logOut());
-		history("/login");
-	};
+  const history = useNavigate();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const dispatch = useDispatch();
+  const logOutHandler = () => {
+    dispatch(logOut());
+    history("/login");
+  };
 
-	return (
-		<div>
-			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-				<Container>
-					<LinkContainer to='/'>
-						<Navbar.Brand>ProShop</Navbar.Brand>
-					</LinkContainer>
-					<Navbar.Toggle aria-controls='basic-navbar-nav' />
-					<Navbar.Collapse id='basic-navbar-nav'>
-						<Nav className='mr-auto'>
-							<LinkContainer to='/cart'>
-								<Nav.Link>
-									<i className='fas fa-shopping-cart'></i>Cart
-								</Nav.Link>
-							</LinkContainer>
+  return (
+    <div>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='mr-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i>Cart
+                </Nav.Link>
+              </LinkContainer>
 
-							{userInfo ? (
-								<NavDropdown id='username' title={userInfo.first_name}>
-									<LinkContainer to='/profile'>
-										<NavDropdown.Item>Profile</NavDropdown.Item>
-									</LinkContainer>
-									<NavDropdown.Item onClick={logOutHandler}>
-										LogOut
-									</NavDropdown.Item>
-								</NavDropdown>
-							) : (
-								<LinkContainer to='/login'>
-									<Nav.Link>
-										{" "}
-										<i className='fas fa-user'></i>Login
-									</Nav.Link>
-								</LinkContainer>
-							)}
-						</Nav>
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
-		</div>
-	);
+              {userInfo ? (
+                <NavDropdown id='username' title={userInfo.first_name}>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logOutHandler}>LogOut</NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    {" "}
+                    <i className='fas fa-user'></i>Login
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
 
 export default Header;
